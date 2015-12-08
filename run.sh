de94266aba31ede34c9e0dc58477ac7190f6e4d2
Ernad Husremovic <hernad@bring.out.ba>
2015-12-08 14:28:10 +0100

    dockerignore data

--- run.sh
+++ run.sh
@@ -7,10 +7,11 @@ CT=xrdp-syncthing
 docker rm -f $CT
 docker run -d \
     -p 8080:8080 -p 3389:3389 \
-    -v $(pwd)/dockerx:/home/dockerx \
     -v $(pwd)/syncthing/config:/syncthing/config \
     -v $(pwd)/data:/syncthing/data \
     --name $CT \
     $IMG
+
+#-v $(pwd)/dockerx:/home/dockerx \
 
 docker exec -ti $CT /bin/bash
